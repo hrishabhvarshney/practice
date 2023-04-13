b67cde581f0863dc99bd55fb34ef0f8aa970038b
Hrishabh Varshney <122474444+hrishabhvarshney@users.noreply.github.com>
2023-04-13 23:03:30 +0530

    Day3

---
+++ src/use_of_trim.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class use_of_trim {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        String y = "      java programming examples      ".trim();
+        System.out.println(" String after Trim : | " + y + "| " );
+    }
+}
