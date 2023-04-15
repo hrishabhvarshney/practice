6d27efd5d54ee8831cc5e327de7f68768b93e088
Hrishabh Varshney <122474444+hrishabhvarshney@users.noreply.github.com>
2023-04-15 16:51:42 +0530

    Day4

---
+++ src/pattern_question2.java
@@ -0,0 +1,27 @@
+/*
+ *
+ * *
+ * * *
+ * * * *
+ * * * * *    */
+
+
+import java.util.Scanner;
+public class pattern_question2 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter no. of rows: ");
+        int row = in.nextInt();
+        System.out.print("Enter no. of cols: ");
+        int col = in.nextInt();
+        pattern2(row, col);
+    }
+    static void pattern2(int r, int c){
+        for (int row = 1; row <= r; row++) {
+            for (int col = 1; col <= row; col++) {
+                System.out.printf("* ");
+            }
+            System.out.println();
+        }
+    }
+}
